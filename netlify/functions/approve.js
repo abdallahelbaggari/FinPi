b75b406b08054664795e6da799001eab590a3f72
abdallahelbaggari <abdallahelbaggari@gmail.com>
2026-03-08 16:56:37 +0100

    Implement payment approval functionality

--- netlify/functions/approve.js
+++ netlify/functions/approve.js
@@ -1,6 +1,38 @@
+const axios = require("axios");
+
 exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Approve function working" })
-  };
+
+  try {
+
+    const data = JSON.parse(event.body);
+    const paymentId = data.paymentId;
+
+    const response = await axios.post(
+      `https://api.minepi.com/v2/payments/${paymentId}/approve`,
+      {},
+      {
+        headers: {
+          Authorization: `Key ${process.env.PI_API_KEY}`
+        }
+      }
+    );
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.data)
+    };
+
+  } catch (error) {
+
+    console.error(error);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: "Approval failed"
+      })
+    };
+
+  }
+
 };
